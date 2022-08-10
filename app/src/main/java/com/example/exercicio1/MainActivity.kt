b4807a573480ebba66f63492534537b8191ccb8f
dpsnqmk <danilopsnts@gmail.com>
2022-08-09 22:03:16 -0300

    add persistindo arquivo

--- app/src/main/java/com/example/exercicio1/MainActivity.kt
+++ app/src/main/java/com/example/exercicio1/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.exercicio1
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileNotFoundException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,19 +17,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnSalvar.setOnClickListener {
-            val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-            val editor = saudacaoPersistencia.edit()
-
-            editor.putString("nome", txtNome.text.toString())
-            editor.putString("tratamento", listaTratamento.selectedItem.toString())
-            editor.apply()
-
+            val data = txtNome.text.toString() + ":" + listaTratamento.selectedItem.toString()
+            saveFileData("saudacao", data)
             Toast.makeText(this, "Salvo com Sucesso", Toast.LENGTH_SHORT).show()
         }
 
         btnExibit.setOnClickListener {
             val intent = Intent(this, SaudacaoActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun saveFileData(filename: String, data: String) {
+        try {
+            val fs = openFileOutput(filename, Context.MODE_PRIVATE)
+            fs.write(data.toByteArray())
+            fs.close()
+        } catch (e: FileNotFoundException) {
+            Log.i("saveFileData", "FileNotFoundException")
+        } catch (e: IOException) {
+            Log.i("saveFileData", "IOException")
         }
     }
 }
--- app/src/main/java/com/example/exercicio1/SaudacaoActivity.kt
+++ app/src/main/java/com/example/exercicio1/SaudacaoActivity.kt
@@ -4,20 +4,40 @@ import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_saudacao.*
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
 
 class SaudacaoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saudacao)
 
-        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
-        val nome = saudacaoPersistencia.getString("nome", "")
-        val tratamento = saudacaoPersistencia.getString("tratamento", "")
-
+        val data = recoveryFileData()
+        val tokenizer = StringTokenizer(data, ":")
+        val nome = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem nome"
+        val tratamento = if (tokenizer.hasMoreTokens()) tokenizer.nextToken() else "Sem tratamento"
         if (tratamento.equals("Sem Tratamento")) {
             lbSaudacao.text = nome
         } else {
             lbSaudacao.text = tratamento + " " + nome
+        }
+
+
+    }
+
+    private fun recoveryFileData(): String {
+        return try {
+            val fi = openFileInput("saudacao")
+            val data = fi.readBytes()
+            fi.close()
+            data.toString()
+            data.toString(Charset.defaultCharset())
+        } catch (e: FileNotFoundException) {
+            ""
+        } catch (e: IOException) {
+            ""
         }
     }
 }
